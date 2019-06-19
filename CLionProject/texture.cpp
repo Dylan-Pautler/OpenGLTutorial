bcde056c2f87568946e97676f27c35c1d87af0a1
Dylan-Pautler <dylan.pautler@outlook.com>
2019-06-19 13:56:55 -0700

    Text commit, adds texture cpp/h and stb_image.c/h
    please work

---
+++ CLionProject/texture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dylan on 6/19/19.
+//
+
+#include "texture.h"
---
+++ CLionProject/texture.h
@@ -0,0 +1,19 @@
+//
+// Created by dylan on 6/19/19.
+//
+
+#ifndef OPENGLTUTORIALPROJECT_TEXTURE_H
+#define OPENGLTUTORIALPROJECT_TEXTURE_H
+
+
+class Texture
+{
+public:
+    Texture();
+
+   virtual ~Texture();
+
+};
+
+
+#endif //OPENGLTUTORIALPROJECT_TEXTURE_H
